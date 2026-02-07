c39bbbd01d913325cf3cd8c360bc73409c767966
Imran Ali <109467587+ftnimran@users.noreply.github.com>
2026-02-08 01:01:55 +0530

    make cards

--- test/src/components/Product.jsx
+++ test/src/components/Product.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import "./Product.css";
 
-const Product = ({ MyName, MyJob }) => {
+const Product = ({ title, description1, description2, price }) => {
   return (
     <div className="Product">
-      <h1>Hi, I'm {MyName.toUpperCase()}</h1>
-      <p>I'm {MyJob}</p>
+      <h2>{title}</h2>
+      <p>{description1}</p>
+      <p>{description2}</p>
+      <div>
+        <h3>{price}</h3>
+        <h3></h3>
+      </div>
     </div>
   );
 };
